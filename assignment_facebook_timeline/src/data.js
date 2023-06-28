0b39d14ccd4f0115f89d97ff505cafc08c3331fc
devripon <developershahadathussainripon@gmail.com>
2023-06-28 14:41:56 +0600

    class_9 assignment facebook timeline

---
+++ assignment_facebook_timeline/src/data.js
@@ -0,0 +1,222 @@
+const posts = [
+	{
+		id: 1,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://www.elegantthemes.com/blog/wp-content/uploads/2023/05/Jasper-AI-Review-and-Guide.jpg",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+	{
+		id: 2,
+		p_content: `অনলাইনে কেনাকাটায় ভিসা কার্ড সেভ করে পেমেন্টে উপভোগ করুন সর্বোচ্চ ২০% পর্যন্ত ছাড়*। 
+                    বিস্তারিত জানতে ভিজিট করুন: https://visa.sslcommerz.com/
+                    *শর্ত প্রযোজ্য। 
+                    *অফার চলবে ৩০ জুন ২০২৩ পর্যন্ত।`,
+		p_media: {
+			photo:
+				"https://sslcommerz.com/wp-content/uploads/2023/02/Payment-gateway-in-Bangladesh.jpg",
+		},
+
+		like_count: 4.5 + "k",
+		comment_count: 200,
+		post_time: "10 min",
+		user: {
+			name: "SSL Commerz",
+			photo: "https://avatars.githubusercontent.com/u/19384040?s=280&v=4",
+		},
+	},
+	{
+		id: 3,
+		p_content: `"𝐆𝐨𝐨𝐠𝐥𝐞 𝐀𝐈 big update June 26, no registration required, 142 languages, works in all countries
+                 Smarter - Learn better - unlimited creative thinking
+                Free to use => https://sites.google.com/view/aipoweredfuture
+             In addition, the new update also supports marketing very well, supports creating feverish marketing content"`,
+		p_media: {
+			photo:
+				"https://nypost.com/wp-content/uploads/sites/2/2023/02/Gogole-stock-chart.jpg",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "BARD AI",
+			photo:
+				"https://www.perfomatix.com/wp-content/uploads/2023/03/BARD-AI.png",
+		},
+	},
+	{
+		id: 4,
+		p_content: `My Workspace Setup🤩🤩`,
+		p_media: {
+			photo:
+				"https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-6/356808134_3559110510990787_5644881613556753681_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGoVe6bFQfNgp3P2tJSPOn1J8lQl4gjcMsnyVCXiCNwy8PJG8QLf1iRIyNp-GYIRRdF0FrxDAvL_wLmNQc_klf9&_nc_ohc=SmpOFwf5olMAX_fgLhT&_nc_zt=23&_nc_ht=scontent.fdac68-2.fna&oh=00_AfDHq4MxB05YU4Uqjjeub6W98E5qet2K-45xD85cGRW7ug&oe=64A092EE",
+		},
+
+		like_count: 40,
+		comment_count: 10,
+		post_time: "30 min",
+		user: {
+			name: "Shahadat Hussain Ripon",
+			photo:
+				"https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-6/308062839_3347638298804677_3111858341122788009_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFhJbFGrJR2Mnx26dS69QUMwYpyvZCXJ2jBinK9kJcnaKIG2K_BFAj92LR-DCq_Sa-Mf7sW2wr_k8Ya76Hc1Oyz&_nc_ohc=4YJI3EzV7kMAX_84S2S&_nc_oc=AQmum9XbX3a3F5Vi2HrwTqYO85ucloqPpiQr4WDc40qyAkyQMLSfCccgHE-TlesdVi0&_nc_zt=23&_nc_ht=scontent.fdac68-2.fna&oh=00_AfDZup4V-YipVrCRfrqI4oPNYRzWrOqLpKAHbDFAzdrKVg&oe=64A171B2",
+		},
+	},
+	{
+		id: 5,
+		p_content: `10 Killer React Animation Libraries.
+
+একটি Web-site কে নান্দনিক করার জন্য আমরা অনেক কিছু করে থাকি।
+বিভিন্ন ডিজাইন আমরা ফলো করি।
+
+একটি  Web-site কে সুন্দর করার 
+অনেক মাধ্যম থাকলেও তার মধ্যে একটি হল Animation ।
+তাই, আজকের এই  দশটি React এর Animation Libraries  আপনাদের অনেক কাজে দিবে
+
+Framer Motion
+Site Link : https://www.framer.com
+
+Pose
+Site Link : https://popmotion.io/pose/
+
+React Spring
+Site Link : https://www.react-spring.dev/
+
+React Reveal
+Site Link : https://www.react-reveal.com/
+
+React Motion
+Site Link : https://www.npmjs.com/package/react-motion
+
+Remotion
+Site Link : https://www.remotion.dev/
+
+React GSAP
+Site Link : https://greensock.com/react/
+
+React Awesome Reveal
+Site Link : https://react-awesome-reveal.morello.dev/
+
+Remotion 
+Site Link : https://www.remotion.dev/
+
+Lottie
+Site Link : https://airbnb.io/lottie/#/
+
+#react`,
+		p_media: {
+			photo:
+				"https://www.syncfusion.com/blogs/wp-content/uploads/2022/08/Top-7-React-Animation-Libraries-in-2022.png",
+		},
+
+		like_count: 40,
+		comment_count: 200,
+		post_time: "Just now",
+		user: {
+			name: "Shahadat Hussain Ripon",
+			photo:
+				"https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-6/308062839_3347638298804677_3111858341122788009_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFhJbFGrJR2Mnx26dS69QUMwYpyvZCXJ2jBinK9kJcnaKIG2K_BFAj92LR-DCq_Sa-Mf7sW2wr_k8Ya76Hc1Oyz&_nc_ohc=4YJI3EzV7kMAX_84S2S&_nc_oc=AQmum9XbX3a3F5Vi2HrwTqYO85ucloqPpiQr4WDc40qyAkyQMLSfCccgHE-TlesdVi0&_nc_zt=23&_nc_ht=scontent.fdac68-2.fna&oh=00_AfDZup4V-YipVrCRfrqI4oPNYRzWrOqLpKAHbDFAzdrKVg&oe=64A171B2",
+		},
+	},
+	{
+		id: 6,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://blog.hubspot.com/hs-fs/hubfs/Google%20Drive%20Integration/How%20Jasper.ai%20Writing%20Assistant%20Works%20%5B+%20Its%20Pros%20and%20Cons%5D-1.png?width=550&height=481&name=How%20Jasper.ai%20Writing%20Assistant%20Works%20%5B+%20Its%20Pros%20and%20Cons%5D-1.png",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "3 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+	{
+		id: 7,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://www.jagoanhosting.com/blog/wp-content/uploads/2023/05/jasper.png",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+	{
+		id: 8,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://www.elegantthemes.com/blog/wp-content/uploads/2023/05/Jasper-AI-Review-and-Guide.jpg",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+	{
+		id: 9,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://www.elegantthemes.com/blog/wp-content/uploads/2023/05/Jasper-AI-Review-and-Guide.jpg",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+	{
+		id: 10,
+		p_content: `𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 is an incredibly powerful tool that can help digital marketers to create high-quality content, engage with customers more effectively, gain valuable insights into customer behavior, and save time. As AI technology continues to evolve, 𝐉𝐚𝐬𝐩𝐞𝐫.𝐀𝐢 will undoubtedly become an even more essential tool for businesses looking to stay ahead of the curve and thrive in an increasingly competitive digital landscape.
+        Try for free: https://www.jas-per.life/`,
+		p_media: {
+			photo:
+				"https://www.elegantthemes.com/blog/wp-content/uploads/2023/05/Jasper-AI-Review-and-Guide.jpg",
+		},
+
+		like_count: 450,
+		comment_count: 100,
+		post_time: "10 min",
+		user: {
+			name: "Jasper AI",
+			photo:
+				"https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_f8f324101efad07fbbf0f0272bfe0cb9/jasper-ai.jpg",
+		},
+	},
+];
