a22667dc0866876e9293cda16eb71e4e1d59b639
devripon <developershahadathussainripon@gmail.com>
2023-06-30 14:09:35 +0600

    team member page team member dynamic with database done

--- assignment_portotheme_class9/src/team/data.js
+++ assignment_portotheme_class9/src/team/data.js
@@ -1,22 +1,117 @@
 const team_category = [
-	"Full Steak Devs",
-	"Front End Devs",
-	"Back End Devs",
-	"Merketing",
-	"SEO",
+  "Full Steak Devs",
+  "Front End Devs",
+  "Back End Devs",
+  "Merketing",
+  "SEO",
 ];
 const team_member = [
-	{
-		id: 1,
-		name: "Ripon Miah",
-		skill: "Mern Steak Developer",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
-		photo: "https://",
-		category: "full steak devs",
-		social: {
-			facebook: "https://www.facebook.com",
-			twitter: "https://twitter.com",
-			linkedin: "https://www.linkedin.com",
-		},
-	},
+  {
+    id: 1,
+    name: "Ripon Miah",
+    skill: "Mern Steak Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://raw.githubusercontent.com/Deveripon/mern_assignments/main/image.png",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 2,
+    name: "Annay Morrison",
+    skill: "Laravel Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://jangada-dideias.pt/wp-content/uploads/2019/08/team-member-02.jpg",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 3,
+    name: "Richard Nixon",
+    skill: "Php Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-11.png",
+    category: "back end devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+    },
+  },
+  {
+    id: 4,
+    name: "Larra Williams",
+    skill: "React Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png",
+    category: "front end devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 5,
+    name: "Criss Morish",
+    skill: "IOS Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-8-1.png",
+    category: "full steak devs",
+    social: {
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 6,
+    name: "Jofree Peter k",
+    skill: "Mern Steak Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://www.lascruces.org/wp-content/uploads/2017/02/team_member_01-540x540.jpg",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 7,
+    name: "Andy Willamson Pitter",
+    skill: "Marketing Expert",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-9.png",
+    category: "Merketing",
+    social: {
+      facebook: "https://www.facebook.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 8,
+    name: "Alexa Andres",
+    skill: "SEO Expert",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://www.lascruces.org/wp-content/uploads/2017/02/team_member_02-540x540.jpg",
+    category: "seo",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
 ];
--- assignment_portotheme_class9/src/team/team.js
+++ assignment_portotheme_class9/src/team/team.js
@@ -1,15 +1,78 @@
+// dynamic team category menu
 const nav_category = document.getElementById("team_catergory");
 let category = `<li class="nav-item active" data-option-value="*">
     <a class="nav-link text-1 text-uppercase active" href="#">Show All</a>
 </li>`;
 
 team_category.map((item, index) => {
-	let splitted = item.split(" ");
-	let joined_item = splitted.join("-").toLowerCase();
+  //String manupulation for data filter
+  let splitted = item.split(" ");
+  let joined_item = splitted.join("-").toLowerCase();
+  //====================================================
 
-	category += `<li class="nav-item" data-option-value=".${joined_item}">
-							<a class="nav-link text-1 text-uppercase" href="#">${item}</a>
-						</li>`;
+  category += `<li class="nav-item" data-option-value=".${joined_item}">
+    <a class="nav-link text-1 text-uppercase" href="#">${item}</a>
+</li>`;
 });
 
 nav_category.innerHTML = category;
+//==============================================================//
+
+//dynamic team members=============================================
+const team_list = document.getElementById("team-list");
+
+let team_members = "";
+team_member.map((member, index) => {
+  //string manupulation for data filter
+  let splitted = member.category.split(" ");
+  let joined_item = splitted.join("-").toLowerCase();
+  //======================================================
+  team_members += `<div class="col-12 col-sm-6 col-lg-3 team_member isotope-item ${joined_item}">
+    <span class="thumb-info thumb-info-hide-wrapper-bg mb-4">
+        <span class="thumb-info-wrapper">
+            <a href="about-me.html">
+                <img id="team-img" src="${
+                  member.photo
+                }" class="img-fluid" alt="" />
+                <span class="thumb-info-title">
+                    <span class="thumb-info-inner">${member.name}</span>
+                    <span class="thumb-info-type">${member.skill}</span>
+                </span>
+            </a>
+        </span>
+        <span class="thumb-info-caption">
+            <span class="thumb-info-caption-text">${member.desc}</span>
+            <span id="social-icon-box" class="thumb-info-social-icons mb-4">
+
+
+                ${
+                  `facebook` in member.social
+                    ? `<a target="_blank" href="${member.social.facebook}"><i
+                        class="fab fa-facebook-f"></i><span>Facebook</span></a>`
+                    : ""
+                }
+                ${
+                  `linkedin` in member.social
+                    ? `<a href="${member.social.linkedin}">
+                    <i class="fab fa-linkedin-in"></i>
+                    <span>Linkedin</span>
+                </a>`
+                    : ""
+                }
+                ${
+                  `twitter` in member.social
+                    ? `<a href="${member.social.twitter}">
+                    <i class="fab fa-twitter"></i>
+                    <span>Twitter</span>
+                </a>`
+                    : ""
+                }
+
+
+            </span>
+        </span>
+    </span>
+</div>`;
+});
+
+team_list.innerHTML = team_members;
