f78060fbd510ffb27d75e95bc6df2e1625142144
deveripon <developershahadathussainripon@gmail.com>
2023-07-04 22:18:23 +0600

    home pag dynamic done

--- assignment_portotheme_class9/src/home/data.js
+++ assignment_portotheme_class9/src/home/data.js
@@ -1,18 +1,19 @@
+//<!-- ========== Clients  Start From Here ========== -->//
 const clients = [
   {
     id: 1,
     brand: "Apple",
-    logo: "https://assets.stickpng.com/images/616056ef76000b00045a7d9d.png",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png",
   },
   {
     id: 2,
     brand: "google",
-    logo: "https://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png",
   },
   {
     id: 3,
     brand: "flickr",
-    logo: "https://assets.stickpng.com/images/6129f9b41f50df0004b7b9d0.png",
+    logo: "https://static.vecteezy.com/system/resources/previews/012/660/855/original/flickr-logo-on-transparent-isolated-background-free-vector.jpg",
   },
   {
     id: 4,
@@ -22,6 +23,149 @@ const clients = [
   {
     id: 5,
     brand: "dribble",
-    logo: "https://assets.stickpng.com/thumbs/62c71bdbb44be1961554a6e6.png",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/3/32/Dribbble_logo.png",
   },
 ];
+//<!-- ========== Clients End From Here ========== -->//
+//<!-- ========== posts Start From Here ========== -->//
+
+const posts = [
+  [
+    {
+      id: 1,
+      title: " Lorem ipsum dolor sit amet consectetur, Corporis, animi?",
+      content:
+        " Lorem, ipsum dolor sit amet consectetur adipisicam accusantium, hic ad ut  numquam?",
+      post_time: {
+        month: "Jan",
+        date: 15,
+      },
+    },
+    {
+      id: 2,
+      title: " Lorem ipsum dolor sit ameg elit. Corporis, animi?",
+      content:
+        " Lorem, ipsum dolor sit amet conssantcere nemom optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Mar",
+        date: 10,
+      },
+    },
+  ],
+  [
+    {
+      id: 3,
+      title: " Lorem ipsum dolor sit amet consectetur, adi animi?",
+      content:
+        " Lorem, ipsum . Maioreshic ad ut vel similiqm optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Feb",
+        date: 18,
+      },
+    },
+    {
+      id: 4,
+      title: " Lorem ipsum dolor sit apis Corporis, animi?",
+      content:
+        " Lorem, ipsum doloe nemo eius veniam optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Jul",
+        date: 23,
+      },
+    },
+  ],
+];
+//<!-- ========== posts End From Here ========== -->//
+//<!-- ========== testimonials Start From Here ========== -->//
+
+const testimonials = [
+  {
+    id: 1,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Ashraful Haque",
+      desg: "Instructor and Sr.Proggrammer",
+      img: "https://avatars.githubusercontent.com/u/63089244?v=4",
+    },
+  },
+  {
+    id: 2,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Zaheed Sabur",
+      desg: "Engineer Of Google Inc",
+      img: "https://media.licdn.com/dms/image/D4E03AQFcu1aYRRKKyA/profile-displayphoto-shrink_800_800/0/1674996318855?e=2147483647&v=beta&t=IRAGEqD2HEgoXevfh333RoLHOA741gl2p0Xq38A2Qhg",
+    },
+  },
+  {
+    id: 3,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Sundar Pichai",
+      desg: "CEO Of Google Inc",
+      img: "https://static.toiimg.com/photo/msid-96204681/96204681.jpg",
+    },
+  },
+];
+//<!-- ========== testimonials End From Here ========== -->//
+//<!-- ==========  Start From Here ========== -->//
+const features = [
+  //left side features
+  [
+    {
+      id: 1,
+      icon: "icon-support",
+      title: "24/7 Customer Support",
+      desc: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 2,
+      icon: "icon-doc",
+      title: "Static Design",
+      desc: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 3,
+      icon: "icon-social-google",
+      title: "Unlimited Fonts Support",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 4,
+      icon: "icon-pencil",
+      title: "Easy To Dynamic",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+  ],
+  // right side features
+  [
+    {
+      id: 5,
+      icon: "icon-layers",
+      title: "Awsome Slider Features",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 6,
+      icon: "icon-user",
+      title: "Awsome Icon Library",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 7,
+      icon: "icon-menu",
+      title: "Interactive Buttons",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 8,
+      icon: "iicon-screen-desktop",
+      title: "Lightbox Perfomence",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+  ],
+];
+//<!-- ==========  End From Here ========== -->//
--- assignment_portotheme_class9/src/home/index.js
+++ assignment_portotheme_class9/src/home/index.js
@@ -1,3 +1,4 @@
+//<!-- ========== Client Logo Slider Start From Here ========== -->//
 const client_slider = document.getElementById("client_slider");
 
 let client = "";
@@ -10,3 +11,155 @@ clients.map((item, index) => {
   </div>`;
 });
 client_slider.innerHTML = client;
+
+//<!-- ========== Client Logo Slider End From Here ========== -->//
+
+//<!-- ========== Latest Post Slider Start From Here ========== -->//
+
+const latest_posts = document.getElementById("latest_posts");
+
+let post = "";
+
+posts.map((item, index) => {
+  post += `
+  <div class="row post_container">
+  <div class=" left_post_container col-lg-6 mb-4 mb-lg-0">
+    <article>
+      <div class="row">
+        <div class="col-auto pr-0">
+          <div class="date">
+            <span class="day font-weight-extra-bold">${item[0].post_time.date}</span>
+            <span class="month text-1">${item[0].post_time.month}</span>
+          </div>
+        </div>
+        <div class="col pl-1">
+          <h4 class="text-primary text-4">
+            <a class="d-block"
+              href="blog-post.html">${item[0].title}</a>
+          </h4>
+          <p class="pr-4 mb-0">
+            ${item[0].content}
+          </p>
+          <a href="https://www.okler.net/"
+            class="read-more text-color-dark font-weight-semibold text-2">read more
+            <i class="fas fa-angle-right position-relative top-1 ml-1"></i></a>
+        </div>
+      </div>
+    </article>
+  </div>
+  <div class="right_post_container col-lg-6">
+    <article>
+      <div class="row">
+        <div class="col-auto pr-0">
+          <div class="date">
+            <span class="day font-weight-extra-bold">${item[1].post_time.date}</span>
+            <span class="month text-1">${item[1].post_time.month}</span>
+          </div>
+        </div>
+        <div class="col pl-1">
+          <h4 class="text-primary text-4">
+            <a class="d-block"
+              href="blog-post.html">${item[1].title}</a>
+          </h4>
+          <p class="pr-4 mb-0">
+            ${item[1].content}
+          </p>
+          <a href="https://www.okler.net/"
+            class="read-more text-color-dark font-weight-semibold text-2">read more
+            <i class="fas fa-angle-right position-relative top-1 ml-1"></i></a>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+  `;
+});
+
+latest_posts.innerHTML = post;
+
+//<!-- ========== Latest Post Slider End From Here ========== -->//
+
+//<!-- ==========  Testimonial Start From Here ========== -->//
+const testimonial_container = document.getElementById("testimonial_container");
+let testimonial = "";
+testimonials.map((item, index) => {
+  testimonial += `
+  <div>
+  <div class="col">
+    <div class="testimonial testimonial-primary">
+      <blockquote>
+        <p class="mb-0">
+          ${item.content}
+        </p>
+      </blockquote>
+      <div class="testimonial-arrow-down"></div>
+      <div class="testimonial-author">
+        <div class="testimonial-author-thumbnail">
+          <img src="${item.client.img}" class="rounded-circle" alt="" />
+        </div>
+        <p>
+          <strong>${item.client.name}</strong>
+          <span>${item.client.desg}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+});
+testimonial_container.innerHTML = testimonial;
+
+//<!-- ==========  Testimonial End From Here ========== -->//
+
+//<!-- ========== Features Start From Here ========== -->//
+//get left container
+const features_left_container = document.querySelector(
+  ".features_left_container"
+);
+// get right container
+const features_right_container = document.querySelector(
+  ".features_right_container"
+);
+
+// fetch data for left container
+let left_feature = "";
+features[0].map((feature, i) => {
+  left_feature += `
+    <div class="feature-box feature-box-style-2">
+    <div class="feature-box-icon">
+      <i class="icons ${feature.icon} text-color-primary"></i>
+    </div>
+    <div class="feature-box-info">
+      <h4 class="font-weight-bold text-4-5 mb-1">
+        ${feature.title}
+      </h4>
+      <p class="mb-4">
+       ${feature.desc}
+      </p>
+    </div>
+    </div>
+`;
+});
+features_left_container.innerHTML = left_feature;
+// fetch data for  right container
+let right_feature = "";
+features[1].map((feature, i) => {
+  right_feature += `
+    <div class="feature-box feature-box-style-2">
+    <div class="feature-box-icon">
+      <i class="icons ${feature.icon} text-color-primary"></i>
+    </div>
+    <div class="feature-box-info">
+      <h4 class="font-weight-bold text-4-5 mb-1">
+        ${feature.title}
+      </h4>
+      <p class="mb-4">
+       ${feature.desc}
+      </p>
+    </div>
+    </div>
+`;
+});
+features_right_container.innerHTML = right_feature;
+
+//<!-- ========== Features End From Here ========== -->//
